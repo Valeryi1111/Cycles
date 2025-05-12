c5a4829d08a4691bcf2e4ff73fe6b23c1896afbf
Valeryi1111 <fursov.valeryi@mail.ru>
2025-05-12 17:03:18 +0200

    problem solving

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,56 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Cycles!");
+
+        System.out.println("Задача 1.");
+        for (int i = 1; i <= 10; i = i + 1) {
+            System.out.println(+i);
+        }
+        System.out.println("Задача 2.");
+        for (int i = 10; i > 0; i = i - 1) {
+            System.out.println(+i);
+        }
+        System.out.println("Задача 3.");
+        for (int i = 0; i <= 17; i = i + 2) {
+            System.out.println(+i);
+        }
+        System.out.println("Задача 4.");
+        for (int i = 10; i >= -10; i = i - 1) {
+            System.out.println(+i);
+        }
+        System.out.println("Задача 5.");
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println(i + " год является високосным.");
+        }
+        System.out.println("Задача 6.");
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.println(+i);
+        }
+        System.out.println("Задача 7.");
+        for (int i = 2; i <= 512; i = i * 2) {
+            System.out.println(+i);
+        }
+        System.out.println("Задача 8.");
+        int amount = 29000;
+        int total = 0;
+        for (int i = 0; i < 12; i++) {
+            total = total + amount;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей.");
+        }
+        System.out.println(total);
+
+        System.out.println("Задача 9.");
+        amount = 29000;
+        total = 0;
+        for (int i = 0; i < 12; i++) {
+            total = total + total / 100;
+            total = total + amount;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей.");
+        }
+        System.out.println("Задача 10.");
+        int number = 2;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + "*" + i + "=" + number * i);
+        }
     }
 }
